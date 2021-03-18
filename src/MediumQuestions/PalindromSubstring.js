67f29cea2ba32041812950100c2fa8a2874f31d8
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2021-03-18 07:36:13 -0700

    palindrom substring

---
+++ src/MediumQuestions/PalindromSubstring.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    const isPalindrom = (s) =>{
+        if (s === '') {return false}
+        let left = 0;
+        let right = s.length - 1;
+        while (left < right) {
+            if (s.charAt(left) !== s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;    }
+
+    let count = 0;
+
+    const traverse = (temp, index) => {
+
+        if (isPalindrom(temp)) {
+            count++;
+        }
+
+        if (index >= s.length) {
+            return;
+        }
+
+        temp += s.charAt(index);
+
+
+        traverse(temp, index+1);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        traverse('', i);
+    }
+    return count;
+};
+
+countSubstrings('abc')
