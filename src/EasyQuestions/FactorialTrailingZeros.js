9fae3a8f37c466c56f95924b09dcfa2f0ec1b58b
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2021-05-07 07:18:08 -0700

    LCQ

---
+++ src/EasyQuestions/FactorialTrailingZeros.js
@@ -0,0 +1,8 @@
+var trailingzero = function (n) {
+  let zeroCount = 0
+  while (n > 4) {
+    n = parseInt(n / 5)
+    zeroCount += n
+  }
+  return zeroCount
+}
---
+++ src/EasyQuestions/HappyNumber.js
@@ -0,0 +1,19 @@
+const isHappy = (n) => {
+  let map = {}
+  let num
+
+  while (!map[n]) {
+    map[n] = true
+
+    num = 0
+    while (n > 0) {
+      num += (n % 10) * (n % 10)
+      n = Math.floor(n / 10)
+    }
+    if (num === 1) {
+      return true
+    }
+    n = num
+  }
+  return false
+}
