bcdd66b8c9da5917da274d7b3d4c1c63f71fc6c3
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2021-05-06 21:20:11 -0700

    LQ

---
+++ src/EasyQuestions/ValidPalindrome.js
@@ -0,0 +1,46 @@
+var isPalindrome = function(s) =>{
+	var start = 0
+    var end = input.length - 1
+    while (start < end) {
+        var s = input.charCodeAt(start)
+        var e = input.charCodeAt(end)
+    	
+    	// continue if its not a letter
+        if (!isLetter(s)) {
+            start++
+            continue
+        }
+        // continue if its not a letter
+        if (!isLetter(e)) {
+            end--
+            continue
+        }
+    
+        if (toLowerCase(s) !== toLowerCase(e)) {
+            return false 
+        } 
+        start++
+        end--
+  }
+	return true	
+}
+
+var isLetter = function(code) {
+    if (((code >= 48) && (code <= 57))  // numbers
+    || ((code >= 65) && (code <= 90))  // uppercase
+    || ((code >= 97) && (code <= 122))) {  // lowercase
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+var toLowerCase = function(code) {
+    if (code >= 65 && code <= 90) {
+        return code + 32    
+    }
+    else {
+        return code
+    }
+}
