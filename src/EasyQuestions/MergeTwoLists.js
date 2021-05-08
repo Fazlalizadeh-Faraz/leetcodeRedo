f0423504d6b6923020a54416b14f8e1e08879987
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2021-05-07 22:13:46 -0700

    added comment

--- src/EasyQuestions/MergeTwoLists.js
+++ src/EasyQuestions/MergeTwoLists.js
@@ -10,35 +10,34 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    
-   
-    if(!l1 || !l2) return (l1? l1:l2);
-    
-    if(l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
+var mergeTwoLists = function (l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
+  }
+}
+
+const mergeTwoListss = function (l1, l2) {
+  const head = new ListNode(null)
+  let curr = head
+  while (l1 && l2) {
+    // choose which value to add
+    if (l1.val < l2.val) {
+      curr.next = l1
+      l1 = l1.next
     } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
+      curr.next = l2
+      l2 = l2.next
     }
-};
-
-
-const mergeTwoListss = function(l1, l2) {
-    const head = new ListNode(null);
-    let curr = head;
-    while (l1 && l2) {
-        if (l1.val < l2.val) {
-            curr.next = l1;
-            l1 = l1.next;
-        } else {
-            curr.next = l2;
-            l2 = l2.next;
-        }
-        curr = curr.next;
-    }
-    curr.next = l1 || l2;
-    return head.next;
-    
-};
+    //iterate
+    curr = curr.next
+  }
+  // for the remaining
+  curr.next = l1 || l2
+  return head.next
+}
--- src/EasyQuestions/PalindromeLinkedList.js
+++ src/EasyQuestions/PalindromeLinkedList.js
@@ -2,10 +2,14 @@ var isPalindrome = function (head) {
   if (head == null || head.next == null) return true
   var slow = head
   var fast = head
+  // this allows us to find the middle
   while (fast.next != null && fast.next.next != null) {
     slow = slow.next
     fast = fast.next.next
   }
+  // by reversing from the middle if
+  // it is palindrome we then know each element should
+  // be the same
   slow.next = reverseList(slow.next)
   slow = slow.next
   while (slow != null) {
