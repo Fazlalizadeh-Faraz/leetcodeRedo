798dbe68a7ef59d4e1ea8a19f777bb7f7088d398
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2021-05-07 22:52:41 -0700

    commented

--- src/EasyQuestions/SortedArrayToBST.js
+++ src/EasyQuestions/SortedArrayToBST.js
@@ -10,15 +10,15 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function(nums) {
-    return convert(0, nums.length - 1)
+var sortedArrayToBST = function (nums) {
+  return convert(0, nums.length - 1)
 
-    function convert(l, r) {
-        if (l > r) return null;
-        const mid = parseInt((l+r)/2);
-        const node = new TreeNode(nums[mid]);
-        node.left = convert(l, mid - 1);
-        node.right = convert(mid + 1, r);
-        return node;
-    }
-};
+  function convert(l, r) {
+    if (l > r) return null
+    const mid = parseInt((l + r) / 2)
+    const node = new TreeNode(nums[mid])
+    node.left = convert(l, mid - 1)
+    node.right = convert(mid + 1, r)
+    return node
+  }
+}
